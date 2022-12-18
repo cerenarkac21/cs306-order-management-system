77eb604da08c6066577beb3adc5c999511d15edc
gurkanyesilbag <79070439+gurkanyesilbag@users.noreply.github.com>
2022-12-19 00:44:12 +0300

    empty table

--- tables.php
+++ tables.php
@@ -1,4 +1,3 @@
-<! ––
 <head>
     <link rel="stylesheet" href="style.css">
     </head>
@@ -11,9 +10,13 @@ include "config.php";
 include "table_view.php";
 
 ?>
+
+<h3>
+	Take Table
+</h3>
 <h4>Please choose which table to take:</h4>
 
-<form action="create_check.php" method="POST">
+<form action="create_tables.php" method="POST">
 <select name="ids">
 
 
@@ -42,6 +45,43 @@ while($id_rows = mysqli_fetch_assoc($myresult))
 </form>
 
 
-<?php include table_view.php?>;
+
+
+
+
+<h3>
+	Empty Table
+</h3>
+<h4>Please choose a table to empty:</h4>
+
+<form action="empty_table.php" method="POST">
+<select name="ids">
+
+
+
+
+<?php
+
+$sql_command = "SELECT table_id from table_entity WHERE occupancy = 1";
+
+$myresult = mysqli_query($db, $sql_command);
+
+
+while($id_rows = mysqli_fetch_assoc($myresult))
+{
+	$table_id = $id_rows['table_id'];
+	$occupancy = $id_rows['occupancy'];
+	echo "<option value=$table_id>". "#" . $table_id. " " . $occupancy. "</option>";
+  
+  
+}
+
+?>
+</select>
+
+<button>SELECT</button>
+</form>
+
+
 
 
