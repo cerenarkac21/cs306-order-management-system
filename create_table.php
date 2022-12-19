c758b8b54a75cb00374bccf9e9d29b3ec63fee59
gurkanyesilbag <79070439+gurkanyesilbag@users.noreply.github.com>
2022-12-19 17:44:22 +0300

    check table relationship

--- create_table.php
+++ create_table.php
@@ -9,14 +9,20 @@ $tableID = $_GET['table_id'];
 if (isset($_POST['ids'])){
 
 $selection_id = $_POST['ids'];
-echo $selection_id;
-$sql_statement = "UPDATE table_entity SET occupancy=1 WHERE table_id=$selection_id";
+$sql_statement = "INSERT INTO Check_entity (delivery_type) VALUES ('in-cite');
+				  UPDATE table_entity SET occupancy=1 WHERE table_id=$selection_id;
+				  )
+";
 
-$result = mysqli_query($db, $sql_statement);
+/*
+$stmt = $conn->prepare("SELECT * FROM users WHERE first_name = ? AND last_name = ?");
+$stmt->bind_param("ss", $first_name, $last_name);;*/
 
+$result = mysqli_multi_query($db, $sql_statement);
 
 
 header ("Location: add_order_panel.php?table=$selection_id");
+
 
 }
 
